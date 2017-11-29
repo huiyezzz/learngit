65d84a5f1358cceee12fb22508b1157290bd5e79
huiye <1011948200@qq.com>
2017-11-29 09:38:06 +0800

    第一次提交

---
+++ src/main/java/priv/mill/base/HelloWord.java
@@ -0,0 +1,10 @@
+package priv.mill.base;
+
+/**
+ * Created by HUIYE on 2017/11/28.
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
